1aa819eee73d2c75ded89581d2c161b3b764fddc
Marcelov1t0r <vitocupertino96@gmail.com>
2023-08-30 20:04:27 -0300

    Modifica um contato da lista telefônica

--- agenda-tele.py
+++ agenda-tele.py
@@ -26,3 +26,14 @@ while(sair==False):
     if(opcao==2):
         for contato in agenda:
             print("\nNome:",contato[0],"Telefone:",contato[1],"\n")
+
+    if(opcao==3):
+        nome=input("\nDigite o nome do contato que deseja editar: ")
+        for i in range(len(agenda)):
+            if(agenda[i][0]==nome):
+                novo_nome=input("Digite o novo nome do contato: ")
+                novo_telefone=input("Digite o novo número: ")
+                agenda[i][0]=novo_nome
+                agenda[i][1]=novo_telefone
+                os.system('cls')
+                break        
